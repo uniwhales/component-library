288f013e1c4f57b3cdb13d711c3aacbee6b2b5b7
Marcin <root@DESKTOP-IME95QQ.localdomain>
2022-02-22 04:47:48 +0100

    update tests

--- stories/text/__test__/Text.test.tsx
+++ stories/text/__test__/Text.test.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import { Text } from '../Text';
-import { Color, THEME } from '../../Theme';
+import { Color, FontSize, THEME } from '../../Theme';
 
 describe('<Text />', () => {
   test('should display Text with SystemGreen color', async () => {
-    const systemGreenText = render(<Text textColor={Color.SystemGreen}>test-text-1</Text>);
+    const systemGreenText = render(
+      <Text textColor={Color.SystemGreen} size={FontSize.Body}>test-text-1</Text>,
+    );
     const element = await systemGreenText.findByText('test-text-1');
     expect(element).toHaveStyle(`color: ${THEME.colors.SystemGreen}`);
+    expect(element).toHaveStyle(`font-size: ${THEME.sizes.Body}`);
   });
 });
