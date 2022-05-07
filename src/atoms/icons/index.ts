cfc2687c9c3aa3c415ee3f497b0e511324b8d5cb
George <goshatravin@gmail.com>
2022-05-07 18:47:16 +0300

    fix

--- src/atoms/icons/index.ts
+++ src/atoms/icons/index.ts
@@ -1,12 +1,3 @@
-// chains
-export * from './chains/aurora';
-export * from './chains/bsc';
-export * from './chains/ethereum';
-export * from './chains/fantom';
-export * from './chains/avalanche';
-export * from './chains/boba';
-export * from './chains/metis';
-export * from './chains/optimism';
 // generalIcons//COLOR
 export * from './generalIcons/fullColorStyle/AlerBotColor';
 export * from './generalIcons/fullColorStyle/EditColor';
