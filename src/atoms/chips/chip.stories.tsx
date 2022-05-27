91d488689f048f7426f5c0fffe1a626e741527d9
shan5742 <asamshan456@gmail.com>
2022-05-27 13:11:58 +0100

    removing hardcoded bool

--- src/atoms/chips/chip.stories.tsx
+++ src/atoms/chips/chip.stories.tsx
@@ -30,8 +30,7 @@ const WithIcon = (isText = true) => (
 
 const Template: ComponentStory<typeof Chip> = (args) => {
   const { children } = args;
-  const editMode = true;
-  return <Chip {...args}>{!editMode ? children : '.....'}</Chip>;
+  return <Chip {...args}>{children}</Chip>;
 };
 export const Primary = Template.bind({});
 export const Secondary = Template.bind({});
