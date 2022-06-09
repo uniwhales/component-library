6fe163bf99477352f0706c5fc72f4ef784ff79c3
George <goshatravin@gmail.com>
2022-06-09 16:06:56 +0300

    better structure

--- src/organisms/actionBar/actionBar.tsx
+++ src/organisms/actionBar/actionBar.tsx
@@ -1,46 +1,14 @@
 import React from 'react';
-import { Styled } from '../../theme';
 import { Select } from '../../atoms/inputs/select';
 import { FilterChip } from '../../atoms/chips/filterChip';
+import { ActionChains, ActionTags, Wrapper } from './actionBar.styles';
+import { ActionBarProps } from './types';
 
-export interface ChainsInterface {
-  id: number;
-  value: string;
-  label: string;
-  isSelected?: boolean;
-}
-export interface FilterInterface {
-  id: number;
-  value: string;
-  label: string;
-  icon: JSX.Element;
-}
-export interface ActionBarProps {
-  filters:FilterInterface[]
-  chains:ChainsInterface[];
-  chainHandler: (e:ChainsInterface) => void;
-  setChip: (e:number) => void;
-  chipValue: number;
-  chainValue:ChainsInterface
-}
 const ActionBarTexts = {
   Select: {
     placeholder: 'All Chains',
   },
 };
-const Wrapper = Styled.div`
-  background: ${(props) => props.theme.containerAndCardShades.SHADE_PLUS_3};
-  border-radius: 12px;
-  padding: 12px 24px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-const ActionTags = Styled.div`
-  display: flex;
-  gap: 24px;
-`;
-const ActionChains = Styled.div``;
 
 export const ActionBar = ({
   filters, chains, chainHandler, setChip, chipValue, chainValue,
