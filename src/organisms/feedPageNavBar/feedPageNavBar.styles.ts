f19e4510846605e61032d529b3f83e3e20421f29
shan5742 <asamshan456@gmail.com>
2022-06-20 07:28:32 +0100

    adding missing ;

--- src/organisms/feedPageNavBar/feedPageNavBar.styles.ts
+++ src/organisms/feedPageNavBar/feedPageNavBar.styles.ts
@@ -6,7 +6,7 @@ export const NavBar = Styled.div`
   align-items: center;
   padding: 16px 24px 8px 24px;
   background: ${({ theme }) => theme.containerAndCardShades.SHADE_PLUS_3};
-  box-shadow: ${({ theme }) => theme.dropShadow.REGULAR}
+  box-shadow: ${({ theme }) => theme.dropShadow.REGULAR};
 `;
 
 export const NavContainer = Styled.div`
