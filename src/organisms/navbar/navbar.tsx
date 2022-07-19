74fbdfdd4b3f629e5f280053d4b1b2d0465a9a49
Marcin Moldrzyk <martinxy80@protonmail.com>
2022-07-19 17:34:04 +0200

    git stash

---
+++ src/organisms/navbar/navbar.tsx
@@ -0,0 +1,49 @@
+import React, { FC } from 'react';
+import { ConnectWalletButton } from '../../atoms/connectWalletButton/connectWalletButton';
+import { ArrowLeftIcon, Identicon } from '../../atoms/icons';
+import { IconWrapper } from '../../atoms/icons/iconWrapper';
+import { Text } from '../../atoms/texts/text';
+import { localTheme } from '../../theme';
+import {
+  NavbarAvatar, NavbarContainer, NavbarLeftSide, NavbarRightSide,
+} from './styles';
+import { NavbarProps } from './types';
+
+export const Navbar: FC<NavbarProps> = ({
+  pageName,
+  plan,
+  onBackButtonClick,
+  leftSideChildren,
+  rightSideChildren,
+  account,
+  onWalletConnectClick,
+}) => {
+  const theme = localTheme();
+  return (
+    <NavbarContainer>
+      <NavbarLeftSide onClick={() => onBackButtonClick()}>
+        <IconWrapper icon={<ArrowLeftIcon />} cursor="pointer" />
+        <Text size="H6-Regular">
+          {pageName}
+        </Text>
+        {leftSideChildren}
+      </NavbarLeftSide>
+      <NavbarRightSide>
+        {rightSideChildren}
+        <Text color={theme.textShades.SHADE_MINUS_1} size="M-Regular">
+          <>
+            {'Plan: '}
+            {plan}
+          </>
+        </Text>
+        {!account
+        && <ConnectWalletButton onClick={() => onWalletConnectClick()} account={account} />}
+        {account && (
+          <NavbarAvatar>
+            <IconWrapper cursor="pointer" icon={<Identicon />} />
+          </NavbarAvatar>
+        )}
+      </NavbarRightSide>
+    </NavbarContainer>
+  );
+};
