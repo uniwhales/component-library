f2d24ab6f1f1a66b1b3332258b92dc18b27ca25b
Marcin Moldrzyk <martinxy80@protonmail.com>
2022-08-16 12:38:56 +0200

    fix types and names

--- src/organisms/actionBar/actionBar.tsx
+++ src/organisms/actionBar/actionBar.tsx
@@ -1,35 +1,35 @@
 import React from 'react';
-import { Placeholder, Select } from '../../atoms/inputs/select';
+import { Placeholder, Select, SelectVariation } from '../../atoms/inputs/select';
 import { FilterChip } from '../../atoms/chips/filterChip';
 import {
   ActionTags, Wrapper,
 } from './actionBar.styles';
-import { ActionBarProps, SelectedInterface } from './types';
+import { ActionBarProps } from './types';
 import { Text } from '../../atoms/texts/text';
 import { IconWrapper } from '../../atoms/icons/iconWrapper';
 import { SettingsBars } from '../../atoms/icons';
 
-export const ActionBar = ({
-  filters, setChip, chipValue, selectValue, selectHandler, selectOptions,
-}: ActionBarProps) => (
+export const ActionBar = <T extends SelectVariation>({
+  chipFilters, setChipFilter, chipFilterValue, selectValue, onSelectChange, selectOptions,
+}: ActionBarProps<T>) => (
   <Wrapper>
     <ActionTags>
-      {filters.map((chip: any) => (
+      {chipFilters.map((chip) => (
         <FilterChip
           key={chip.id}
-          onClick={setChip}
-          isOn={chipValue === chip.id}
-          {...chip}
+          onClick={setChipFilter}
+          isOn={chipFilterValue === chip.id}
+          id={chip.id}
         >
           <Text size="S-Regular">{chip.label}</Text>
         </FilterChip>
       ))}
     </ActionTags>
 
-    <Select
-      value={selectValue}
-      onChange={(e: SelectedInterface) => selectHandler(e)}
-      options={selectOptions}
+    <Select<T>
+      selectValue={selectValue}
+      onSelectChange={(e) => onSelectChange && onSelectChange(e)}
+      selectOptions={selectOptions}
       isMulti
       isSearchable
       isXL
@@ -40,8 +40,8 @@ export const ActionBar = ({
           <IconWrapper height="20px" width="20px" icon={<SettingsBars />} />
           <Text size="S-Regular">Filter Tx types and Chains</Text>
         </Placeholder>
-)}
+        )}
     />
 
   </Wrapper>
-);
+  );
--- src/organisms/actionBar/types.ts
+++ src/organisms/actionBar/types.ts
@@ -1,45 +1,18 @@
 import { CardProps } from '../../atoms/card/types';
+import { FilterChipProps } from '../../atoms/chips/filterChip';
+import {
+  SelectProps, SelectVariation,
+} from '../../atoms/inputs/select';
 
-export interface Option {
-  value: string,
-  label: string,
-  icon?: JSX.Element,
-  id: number;
-}
+export type ChipFilterProps = Pick<FilterChipProps, 'icon' | 'isOn' | 'id'> & {
+  label: string
+};
 
-export interface GroupOptionInterface {
-  label: string;
-  options: Option[]
-}
-
-export interface ChainsInterface {
-  id: number;
-  value: string;
-  label: string;
-  isSelected?: boolean;
-}
-
-export interface SelectedInterface {
-  id: number;
-  value: string;
-  label: string;
-  icon?: JSX.Element;
-  isSelected?: boolean;
-}
-export interface FilterInterface {
-  id: number;
-  value: string;
-  label: string;
-  icon: JSX.Element;
-}
-export interface ActionBarProps {
-  filters: FilterInterface[]
-  selectHandler: (e: SelectedInterface) => void;
-  selectValue: SelectedInterface;
-  setChip: (e: number) => void;
-  chipValue: number;
-  selectOptions: GroupOptionInterface[] | Option[];
-}
+export type ActionBarProps<T extends SelectVariation> = {
+  chipFilters: ChipFilterProps[]
+  setChipFilter: (e: number) => void;
+  chipFilterValue: number;
+} & Pick<SelectProps<T>, 'selectValue' | 'onSelectChange' | 'selectOptions'>;
 
 export interface MobileRHSActionBarProps extends CardProps {
   left: JSX.Element | string
