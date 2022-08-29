786463cd18aeed01fa41919fcd796c48e930db71
Marcin Moldrzyk <martinxy80@protonmail.com>
2022-08-29 15:52:49 +0200

    add stories

--- src/molecules/stickyActionBar/stickyActionBar.stories.tsx
+++ src/molecules/stickyActionBar/stickyActionBar.stories.tsx
@@ -1,29 +1,84 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import React from 'react';
-import { TokenPoolCard } from './tokenPoolCard';
+import { Feed, NewsStandard, SettingsBars } from '../../atoms/icons';
+import { Checkbox } from '../../atoms/inputs/checkbox';
+import { Select } from '../../atoms/inputs/select';
+import { Text } from '../../atoms/texts/text';
+import { groupTagOptions } from '../../organisms/navbar/mockData';
 import { Styled } from '../../theme';
-import { fakePoolData } from './data';
+import { CheckboxSize } from '../../utils/getSize';
+import { StickyActionBar } from './stickyActionBar';
 
 const Wrapper = Styled.div`
   display: flex;
   flex-direction: column;
-  gap: 16px;
+  height: 200vh;
+  gap: 15px;
+  background-color: ${({ theme }) => theme.containerAndCardShades.NEUTRAL_SHADE_0};
 `;
+
+const Dummy = Styled.div`
+  width: 100%;
+  height: 400px;
+  background-color: rgb(0, 0 ,0, 0.5);
+`;
+
+const FeedLeftSideChildren = Styled.div`
+  display: flex;
+  justify-content:center;
+  align-items:center;
+  gap: 24px;
+  margin-left: 24px;
+`;
+
 export default {
-  title: 'Molecules/TokenPoolCard',
-  component: TokenPoolCard,
-  argTypes: {},
-} as ComponentMeta<typeof TokenPoolCard>;
+  title: 'Molecules/StickyActionBar',
+  component: StickyActionBar,
+} as ComponentMeta<typeof StickyActionBar & { multi?: boolean }>;
 
-const Template: ComponentStory<typeof TokenPoolCard> = () => (
+const Template: ComponentStory<typeof StickyActionBar> = (args) => (
   <Wrapper>
-    {fakePoolData.map((
-      item,
-      index,
-    ) => <TokenPoolCard data={item} index={index + 1} />)}
-
+    <Dummy />
+    <StickyActionBar
+      {...args}
+      icon={<SettingsBars />}
+      left={(
+        <FeedLeftSideChildren>
+          <Select
+            selectOptions={groupTagOptions}
+            placeholder="Select Group Tags"
+            onSelectChange={() => {}}
+            selectValue={groupTagOptions[0]}
+            isXL
+            readOnly={false}
+            isMulti={false}
+          />
+          <Text href="TODO-ADD-LINK" size="S-Regular">
+            <>
+              Following:
+              {' '}
+              {1000}
+            </>
+          </Text>
+        </FeedLeftSideChildren>
+      )}
+      right={(
+        <Checkbox
+          disabled={false}
+          onClick={() => {}}
+          selected
+          size={CheckboxSize.Small}
+        />
+      )}
+      withTransform
+    />
+    <Dummy />
+    <StickyActionBar {...args} icon={<NewsStandard />} index={2} withTransform />
+    <Dummy />
+    <Dummy />
+    <Dummy />
+    <Dummy />
   </Wrapper>
-
 );
 
-export const PrimaryTabsGroups = Template.bind({});
+export const Primary = Template.bind({});
