30244f404472cc39a12ee3b60b74405f12125afa
Marcin Moldrzyk <martinxy80@protonmail.com>
2022-09-08 11:29:55 +0200

    added fade pop up animation for icons on sticky bars

--- src/atoms/animations/fades.tsx
+++ src/atoms/animations/fades.tsx
@@ -27,3 +27,21 @@ export const keyframeFadeIn = keyframes`
 export const FadeIn = Styled.div`
   animation: ${keyframeFadeIn} 2s ease-in;
 `;
+
+export const keyframeFadePopUp = keyframes`
+  from {
+    opacity: 0;
+    transform: scale(0.5);
+  }
+  85% {
+    transform: scale(1.2);
+  }
+  to {
+    opacity: 1;
+    transform: scale(1);
+  }
+`;
+
+export const FadePopUp = Styled.div`
+  animation: ${keyframeFadePopUp} 0.3s ease-in;
+`;
