bde9e88a55ba24b3d51f4489d3eccd77015fcdb0
ItsMarcin <martinxy80@protonmail.com>
2022-12-07 07:18:34 +0100

    fix select

--- src/atoms/inputs/select.tsx
+++ src/atoms/inputs/select.tsx
@@ -131,9 +131,11 @@ const colourStyles: StylesConfig<StyledProps, false> = {
     boxShadow: 'none',
     borderRadius: menuIsOpen && isFocused ? '12px 12px 0 0 ' : '12px',
     height: '40px',
-    svg: {
-      transform: menuIsOpen ? 'rotateZ(-180deg)' : undefined,
-      cursor: 'pointer',
+    'div:nth-child(2)': {
+      svg: {
+        transform: menuIsOpen ? 'rotateZ(-180deg)' : undefined,
+        cursor: 'pointer',
+      },
     },
     color: isFocused ? theme.colors.system.WHITE : theme.textShades.SHADE_MINUS_2,
     fontWeight: isFocused ? 'bold' : 'normal',
