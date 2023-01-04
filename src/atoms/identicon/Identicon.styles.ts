63a24d3a8cca54fa6ee96fb2689b3f96f37f98e7
shan5742 <asamshan456@gmail.com>
2023-01-04 17:04:22 +0000

    finalising identicon

--- src/atoms/identicon/Identicon.styles.ts
+++ src/atoms/identicon/Identicon.styles.ts
@@ -13,17 +13,17 @@ export const Container = Styled.div<Pick<IdenticonProps, 'hasInteraction' | 'siz
   border-radius: 50%;
   box-sizing: border-box;
   cursor: ${({ hasInteraction }) => hasInteraction && 'pointer'};
-  background: ${({ theme, gradientBorder }) => (gradientBorder ? `linear-gradient(${theme.containerAndCardShades.SHADE_PLUS_1}, ${theme.containerAndCardShades.SHADE_PLUS_1}) padding-box,${theme.gradients.primary.MAIN_BLUE_GRADIENT_FLIPPED} border-box` : `linear-gradient(${theme.containerAndCardShades.SHADE_PLUS_3}, ${theme.containerAndCardShades.SHADE_PLUS_3}) padding-box,${theme.gradients.secondary.SHADE_HORIZONTAL} border-box`)};
+  background: ${({ theme, gradientBorder, size }) => (size === 'small' ? null : gradientBorder ? `linear-gradient(${theme.containerAndCardShades.SHADE_PLUS_1}, ${theme.containerAndCardShades.SHADE_PLUS_1}) padding-box,${theme.gradients.primary.MAIN_BLUE_GRADIENT_FLIPPED} border-box` : `linear-gradient(${theme.containerAndCardShades.SHADE_PLUS_3}, ${theme.containerAndCardShades.SHADE_PLUS_3}) padding-box,${theme.gradients.secondary.SHADE_VERTICAL} border-box`)};
   border: 1px solid transparent;
   &:hover {
     border: ${({ hasInteraction }) => hasInteraction && '2px solid transparent'};
-    background: ${({ theme, gradientBorder }) => !gradientBorder && `linear-gradient(${theme.containerAndCardShades.BG_SHADE_PLUS_4}, ${theme.containerAndCardShades.BG_SHADE_PLUS_4}) padding-box,${theme.gradients.secondary.RADIAL_LIGHT} border-box`};
+    background: ${({ theme, gradientBorder, hasInteraction }) => hasInteraction && !gradientBorder && `linear-gradient(${theme.containerAndCardShades.BG_SHADE_PLUS_4}, ${theme.containerAndCardShades.BG_SHADE_PLUS_4}) padding-box,${theme.gradients.secondary.RADIAL_LIGHT} border-box`};
   };
    /* On mobile we do not have hover effects   */
   @media (min-width: ${DeviceWidth.tablet}) {
     :hover {
       border: ${({ hasInteraction }) => hasInteraction && '2px solid transparent'};
-      background: ${({ theme, gradientBorder }) => !gradientBorder && `linear-gradient(${theme.containerAndCardShades.BG_SHADE_PLUS_4}, ${theme.containerAndCardShades.BG_SHADE_PLUS_4}) padding-box,${theme.gradients.secondary.RADIAL_LIGHT} border-box`};
+      background: ${({ theme, gradientBorder, hasInteraction }) => hasInteraction && !gradientBorder && `linear-gradient(${theme.containerAndCardShades.BG_SHADE_PLUS_4}, ${theme.containerAndCardShades.BG_SHADE_PLUS_4}) padding-box,${theme.gradients.secondary.RADIAL_LIGHT} border-box`};
     };
   }
 
