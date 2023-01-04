afae02d1075c4c062002aea7e6f2339383dc51ca
shan5742 <asamshan456@gmail.com>
2023-01-04 13:15:19 +0000

    adding new font sizes

--- src/atoms/texts/textGenerator.ts
+++ src/atoms/texts/textGenerator.ts
@@ -1,18 +1,20 @@
 export const P_FONTSIZE = Object.freeze({
-  XXXS: 8,
-  XXS: 10,
-  XS: 12,
+  Overline: 8,
+  Tiny: 10,
+  Caption: 12,
   S: 14,
-  M: 16,
-  L: 18,
+  Button: 14,
+  M: 14,
+  L: 16,
 });
 export const P_LINE_HEIGHT = Object.freeze({
-  XXXS: 12,
-  XXS: 12,
-  XS: 16,
-  S: 20,
+  Overline: 10,
+  Tiny: 12,
+  Caption: 16,
+  S: 18,
+  Button: 20,
   M: 20,
-  L: 24,
+  L: 20,
 });
 export const H_FONTSIZE = Object.freeze({
   H1: 44,
@@ -20,20 +22,18 @@ export const H_FONTSIZE = Object.freeze({
   H3: 32,
   H4: 24,
   H5: 20,
-  H6: 18,
+  H6: 16,
 });
 export const H_LINE_HEIGHT = Object.freeze({
   H1: 54,
   H2: 54,
   H3: 44,
   H4: 32,
-  H5: 32,
+  H5: 24,
   H6: 24,
 });
 
 export const FONTWEIGHT = Object.freeze({
-  Semibold: 600,
   Bold: 700,
   Regular: 400,
-  light: 300,
 });
