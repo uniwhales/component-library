55d71702a1571a44b0439fe5cb2839256c99ee8c
ItsMarcin <martinxy80@protonmail.com>
2023-02-15 13:52:32 +0100

    add gap prop to basic flex components

--- src/atoms/common/flex.tsx
+++ src/atoms/common/flex.tsx
@@ -1,10 +1,12 @@
 import { Styled } from '../../theme';
 
-export const Column = Styled.div`
+export const Column = Styled.div<{ gap?: string }>`
     display: flex;
     flex-direction: column;
+    gap: ${({ gap }) => gap || '0px'}
 `;
 
-export const Row = Styled.div`
+export const Row = Styled.div<{ gap?: string }>`
     display: flex;
+    gap: ${({ gap }) => gap || '0px'}
 `;
