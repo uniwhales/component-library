4b05f1d0fb176a5453f45d135bbda09fcf67f365
shan5742 <asamshan456@gmail.com>
2023-02-15 14:44:50 +0000

    save point, clenup

--- src/organisms/feedPageScrollableContent/types.ts
+++
@@ -1,8 +0,0 @@
-import { ReactChild } from 'react';
-
-export interface FeedPageScrollableContentProps {
-  children: ReactChild | ReactChild[];
-  newUpdates?: { data: any[], paging: {} };
-  onShowNew?: () => void;
-  offset?: string;
-}
