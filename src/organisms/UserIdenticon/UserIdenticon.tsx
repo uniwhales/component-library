326b57347953156d573cd008f8cb41f2af12acda
ItsMarcin <martinxy80@protonmail.com>
2023-02-20 13:06:50 +0100

    new user identicon

---
+++ src/organisms/UserIdenticon/UserIdenticon.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { ConnectButtonProps } from '../../atoms/connectWalletButton/types';
+import { IdenticonComponent } from '../../atoms/identicon/Identicon';
+import { Text } from '../../atoms/texts/text';
+import { shortenAddressTo11Chars } from '../../utils/shortenAddress';
+import { UserMenu } from '../navbar/components/UserMenu/UserMenu';
+import { UserIdenticonWrapper } from './style';
+
+export type UserIdenticonProps = { plan?: string, onWalletConnectClick: ConnectButtonProps['onClick'], account?: string };
+
+export const UserIdenticon = ({
+  account,
+  onWalletConnectClick,
+  plan,
+}: UserIdenticonProps) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <>
+      <UserIdenticonWrapper>
+        <IdenticonComponent
+          hasInteraction
+          size="big"
+          id="user-identicon"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        />
+        {account && <Text size="Tiny-Regular">{shortenAddressTo11Chars(account)}</Text>}
+      </UserIdenticonWrapper>
+      <UserMenu
+        isMenuOpen={isMenuOpen}
+        onWalletConnectClick={(e) => {
+          onWalletConnectClick(e);
+        }}
+        plan={plan ?? ''}
+        account={account}
+      />
+    </>
+
+  );
+};
---
+++ src/organisms/UserIdenticon/style.tsx
@@ -0,0 +1,10 @@
+import { Styled } from '../../theme';
+
+export const UserIdenticonWrapper = Styled.div`
+    position: fixed;
+    top: 43px;
+    right: 115px;
+    display: flex;
+    align-items: center;
+    gap: 8px;
+`;
