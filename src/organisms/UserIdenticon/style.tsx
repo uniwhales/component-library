c9496c78c1c61404e06e703a80edf1068238a50e
Marcin Moldrzyk <martinxy80@protonmail.com>
2023-02-28 01:48:03 +0100

    fix right

--- src/organisms/UserIdenticon/style.tsx
+++ src/organisms/UserIdenticon/style.tsx
@@ -3,7 +3,7 @@ import { Styled } from '../../theme';
 export const UserIdenticonWrapper = Styled.div`
     position: fixed;
     top: 43px;
-    right: 115px;
+    right: 45px;
     display: flex;
     align-items: center;
     gap: 8px;
