fe13084926c0d740c95e03d2ae625300c5e1691b
ItsMarcin <martinxy80@protonmail.com>
2023-03-21 21:57:47 +0200

    move trending flame icon

--- src/atoms/icons/iconWrapper.stories.tsx
+++ src/atoms/icons/iconWrapper.stories.tsx
@@ -151,7 +151,7 @@ import {
   CurveFilled, ZeroXFilled, OneInchV3Filled, OneInchV2Filled, DogecoinFilled,
   BalencerFilled, EvmosFilled, BancorFilled, RedCross, ChainScan,
   CrownStandard, ShareLinkStandard, LeaderboardStandard, Suggested,
-  Follow, PlanetStandard, MultipartFormStandard, Timestamp, Flame,
+  Follow, PlanetStandard, MultipartFormStandard, Timestamp, TrendingFlame,
 } from '.';
 import { Collection } from './generalIcons/filledStyle/Collection';
 import { EvmosColor } from './chains/fullColorStyle/EvmosColor';
@@ -447,7 +447,7 @@ const Template9: ComponentStory<typeof IconWrapper> = () => (
     <IconWrapper name="Filter Tokens" icon={<FilterTokens />} />
     <IconWrapper name="Filter Tx" icon={<FilterTx />} />
     <IconWrapper name="Timestamp" icon={<Timestamp />} />
-    <IconWrapper name="Flame" icon={<Flame />} />
+    <IconWrapper name="TrendingFlame" icon={<TrendingFlame />} />
   </>
 );
 
