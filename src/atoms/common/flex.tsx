e999a564316fd9d27fa84c243c7d6215e4d88537
ItsMarcin <martinxy80@protonmail.com>
2023-03-27 23:03:30 +0300

    fixes and flex improvements

--- src/atoms/common/flex.tsx
+++ src/atoms/common/flex.tsx
@@ -1,12 +1,15 @@
+import { CSSProperties } from 'styled-components';
 import { Styled } from '../../theme';
 
-export const Column = Styled.div<{ gap?: string }>`
+export type FlexProps = Pick<CSSProperties, 'justifyContent' | 'alignItems' | 'justifyItems' | 'gap' >;
+
+export const Column = Styled.div<FlexProps>`
     display: flex;
     flex-direction: column;
     gap: ${({ gap }) => gap || '0px'}
 `;
 
-export const Row = Styled.div<{ gap?: string }>`
+export const Row = Styled.div<FlexProps>`
     display: flex;
     gap: ${({ gap }) => gap || '0px'}
 `;
