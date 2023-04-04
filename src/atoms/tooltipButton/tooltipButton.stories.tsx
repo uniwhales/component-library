1cec8da6b469db71fb429f23205e11d60088f260
shan5742 <asamshan456@gmail.com>
2023-04-04 15:30:14 +0100

    tooltip story

--- src/atoms/tooltipButton/tooltipButton.stories.tsx
+++ src/atoms/tooltipButton/tooltipButton.stories.tsx
@@ -28,7 +28,7 @@ export default {
 const Template: ComponentStory<typeof TooltipButton> = () => (
   <Wrapper>
     <TooltipButton
-      content={(<div>hello</div>)}
+      content={(<div>Additional content, triggered on click</div>)}
     />
   </Wrapper>
 );
