ca5f7892d7877eb5bd6323b675a0069486739815
Marcin Moldrzyk <martinxy80@protonmail.com>
2023-04-28 09:40:00 +0200

    fixes

--- src/molecules/controlledTooltip/types.ts
+++ src/molecules/controlledTooltip/types.ts
@@ -4,4 +4,5 @@ import { ReactNode } from 'react';
 export type ControlledTooltipProps = {
   target: ReactNode;
   content: ReactNode;
+  padding?: number;
 } & Pick<PopoverProps, 'withinPortal' | 'disabled' | 'zIndex' | 'withArrow' | 'arrowSize' | 'position' | 'opened'>;
