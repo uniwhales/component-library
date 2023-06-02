322b948852147a10fc675147b8f5c9219efa1858
shan5742 <asamshan456@gmail.com>
2023-06-02 09:27:07 +0100

    dev version

--- src/atoms/subscriptNumber/subscript.stories.tsx
+++ src/atoms/subscriptNumber/subscript.stories.tsx
@@ -1,22 +1,9 @@
 import React from 'react';
 import { ComponentMeta, Story } from '@storybook/react';
-import { BodySizes, HeaderSizes } from '../texts/types';
+import { BodySizes } from '../texts/types';
 import { Subscript } from './subscript';
 import { Text } from '../texts/text';
 import { Row } from '../common/flex';
-
-const headerSizes: HeaderSizes[] = ['H1-Regular',
-  'H1-Bold',
-  'H2-Regular',
-  'H2-Bold',
-  'H3-Regular',
-  'H3-Bold',
-  'H4-Regular',
-  'H4-Bold',
-  'H5-Regular',
-  'H5-Bold',
-  'H6-Regular',
-  'H6-Bold'];
 
 const bodySizes: BodySizes[] = [
   '16-Regular',
@@ -34,7 +21,7 @@ const bodySizes: BodySizes[] = [
 ];
 
 interface TemplateSizes {
-  list: BodySizes[] | HeaderSizes[],
+  list: BodySizes[],
 }
 
 export default {
@@ -69,13 +56,7 @@ export const BodyText = Template.bind({});
 BodyText.args = {
   list: bodySizes,
 };
-HeadingsAndTitles.args = {
-  list: headerSizes,
-};
 
 BodyText.parameters = {
-  backgrounds: { default: 'dark' },
-};
-HeadingsAndTitles.parameters = {
   backgrounds: { default: 'dark' },
 };
