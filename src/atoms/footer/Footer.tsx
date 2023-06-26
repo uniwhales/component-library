5995032727e6abd7a1d3703a12098c9b43344059
shan5742 <asamshan456@gmail.com>
2023-06-26 15:02:17 +0100

    [FE-1401] - update footer content

--- src/atoms/footer/Footer.tsx
+++ src/atoms/footer/Footer.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { Text } from '../texts/text';
 import { FooterComponent } from './Footer.styles';
 import { localTheme } from '../../theme';
-import { FooterLogo } from './FooterLogo';
 
 export const Footer = () => {
   const theme = localTheme();
   return (
     <FooterComponent>
-      <FooterLogo />
-      <Text color={theme.textShades.SHADE_MINUS_2} size="10-Regular">UniWhales DAO ©2022</Text>
+      <Text color={theme.textShades.SHADE_MINUS_2} size="10-Regular">Cielo | ©2023</Text>
     </FooterComponent>
   );
 };
