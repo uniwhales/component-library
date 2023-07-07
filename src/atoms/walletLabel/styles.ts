7fbfc871e12d173194b462aa7b4be42555101fce
shan5742 <asamshan456@gmail.com>
2023-07-07 10:16:57 +0100

    [FE-1394] - finalise atoms migration to transient props

--- src/atoms/walletLabel/styles.ts
+++
@@ -1,6 +0,0 @@
-import { styled } from 'styled-components';
-
-export const Container = styled.div<{ breakWord:boolean, width?:string }>`
-  word-break: ${({ breakWord }) => breakWord && 'break-word'};
-  width ${({ width }) => width && width};
-`;
--- src/atoms/walletLabel/types.ts
+++
@@ -1,7 +0,0 @@
-import { TextPropsBase } from '../shortText/types';
-
-export type WalletLabelProps = TextPropsBase & {
-  label?: string;
-  address: string;
-  width?: string;
-};
