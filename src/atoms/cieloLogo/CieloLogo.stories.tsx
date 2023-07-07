34be291b9ddc145ec1ef66c1ef56594cf5c06605
shan5742 <asamshan456@gmail.com>
2023-07-07 08:58:22 +0100

    [FE-1394] - migrate cielo logo to transient props

--- src/atoms/cieloLogo/CieloLogo.stories.tsx
+++ src/atoms/cieloLogo/CieloLogo.stories.tsx
@@ -14,7 +14,7 @@ const Template = (args: React.JSX.IntrinsicAttributes & LogoProps) => {
   const [text, setText] = useState(true);
   return (
     <>
-      <CieloLogo {...args} noText={text} />
+      <CieloLogo {...args} $noText={text} />
       <button type="button" onClick={() => setText(!text)}>{text ? 'Full logo' : 'Symbol only logo'}</button>
     </>
 
@@ -24,15 +24,15 @@ const Template = (args: React.JSX.IntrinsicAttributes & LogoProps) => {
 export const Standard: Story = {
   render: (args, { globals: { backgrounds } }) => {
     const isDark = backgrounds?.value === '#191B20';
-    return <Template {...args} night={isDark} />;
+    return <Template {...args} $night={isDark} />;
   },
 };
 export const Beta: Story = {
   render: (args, { globals: { backgrounds } }) => {
     const isDark = backgrounds?.value === '#191B20';
-    return <Template {...args} night={isDark} />;
+    return <Template {...args} $night={isDark} />;
   },
 };
 Beta.args = {
-  isBeta: true,
+  $isBeta: true,
 };
