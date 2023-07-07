f1ca10270e7f1739a69f7009a0f6168cb82f77b1
shan5742 <asamshan456@gmail.com>
2023-07-07 09:06:07 +0100

    [FE-1394] - migrate connect wallet button to transient props

--- src/atoms/connectWalletButton/connectWalletButton.styles.ts
+++ src/atoms/connectWalletButton/connectWalletButton.styles.ts
@@ -2,7 +2,7 @@ import { css, styled } from 'styled-components';
 import { tablet } from '../../layouts/breakpoints';
 import { ConnectButtonProps } from './types';
 
-export const ConnectButton = styled.button<ConnectButtonProps & { isConnected: boolean }>`
+export const ConnectButton = styled.button<ConnectButtonProps & { $isConnected: boolean }>`
   svg {
     cursor: pointer;
     fill: ${({ theme }) => theme.textShades.SHADE_MINUS_3};
@@ -15,7 +15,7 @@ export const ConnectButton = styled.button<ConnectButtonProps & { isConnected: b
   font-family: inherit;
   height: 40px;
   background-color: ${() => 'transparent'};
-  border: ${({ theme, isConnected }) => (isConnected ? 'none' : `2px solid ${theme.colors.primary.MAIN_BLUE}`)};
+  border: ${({ theme, $isConnected }) => ($isConnected ? 'none' : `2px solid ${theme.colors.primary.MAIN_BLUE}`)};
   cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
   transition: color 300ms, fill 300ms, background 300ms ease-in-out;
 
@@ -26,26 +26,26 @@ export const ConnectButton = styled.button<ConnectButtonProps & { isConnected: b
     gap: 5px;
   };
 
-  ${({ isConnected, theme }) => css`
+  ${({ $isConnected, theme }) => css`
     &:hover {
       p {
-        color: ${isConnected ? theme.colors.system.RED : theme.colors.system.WHITE};
+        color: ${$isConnected ? theme.colors.system.RED : theme.colors.system.WHITE};
       };
       svg {
         fill: ${theme.colors.system.WHITE};
       };
-      background: ${isConnected ? 'transparent' : theme.colors.system.GREEN};
-      border: ${isConnected ? 'none' : `2px solid ${theme.colors.system.GREEN}`};
+      background: ${$isConnected ? 'transparent' : theme.colors.system.GREEN};
+      border: ${$isConnected ? 'none' : `2px solid ${theme.colors.system.GREEN}`};
     }
   `}
 
-  ${tablet(css<{ isConnected: boolean }>`
+  ${tablet(css<{ $isConnected: boolean }>`
     &:active {
       p {
         color: ${({ theme }) => theme.colors.system.WHITE};
       };
-      border: ${({ theme, isConnected }) => (isConnected ? 'none' : `2px solid ${theme.colors.system.GREEN}`)};
-      background: ${({ theme, isConnected }) => (isConnected ? theme.colors.system.RED : theme.colors.system.GREEN)};
+      border: ${({ theme, $isConnected }) => ($isConnected ? 'none' : `2px solid ${theme.colors.system.GREEN}`)};
+      background: ${({ theme, $isConnected }) => ($isConnected ? theme.colors.system.RED : theme.colors.system.GREEN)};
     }
   `)};
 `;
