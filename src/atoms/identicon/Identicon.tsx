7e03a72f2dc6fd0c839c347e308e0fa7231b10ee
ItsMarcin <martinxy80@protonmail.com>
2023-07-11 01:17:08 +0100

    wip

--- src/atoms/identicon/Identicon.tsx
+++ src/atoms/identicon/Identicon.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import Identicon from 'react-hooks-identicons';
 import { Container } from './Identicon.styles';
 import { IdenticonProps } from './types';
 
 export const IdenticonComponent = ({
   size,
-  id,
   hasInteraction,
   onClick,
   href,
@@ -22,6 +20,6 @@ export const IdenticonComponent = ({
     target={target}
     onMouseEnter={() => onMouseEnter && onMouseEnter()}
   >
-    <Identicon count={5} string={id} size={size === 'big' ? 22 : 12} />
+    <div />
   </Container>
 );
