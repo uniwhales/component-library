a1c82ecf4c7059d386f7ad48010e5481572a93c7
shan5742 <asamshan456@gmail.com>
2023-07-30 06:04:52 +0100

    [FE-1528] - fix dom nesting issue

--- src/molecules/labeled/select/labeledSelect.tsx
+++ src/molecules/labeled/select/labeledSelect.tsx
@@ -39,38 +39,38 @@ export const LabeledSelect = <T extends SelectVariation>(
   return (
     <LabeledSelectWrapper>
       {label && (
-      <Text size={labelFontSize ?? '12-Regular'} color={!isDisabled && hover ? textShades.SHADE_MINUS_3 : labelColor || (isDisabled ? textShades.SHADE_MINUS_1 : textShades.SHADE_MINUS_2)}>
-        <>
-          <Row gap="4px" alignItems="center">
-            {icon && (
-            <IconWrapper
-              height={iconSize || '14px'}
-              width={iconSize || '14px'}
-              icon={icon}
-            />
-            )}
-            {label}
-          </Row>
-          {required && (<Required disabled={!!isDisabled}>*</Required>)}
-        </>
-      </Text>
-      )}
-      {labels && labels.map((l) => (
-        <Text key={l.label} size={l?.fontSize ?? '12-Regular'} color={!isDisabled && hover ? textShades.SHADE_MINUS_3 : l.color ?? textShades.SHADE_MINUS_1}>
+      <Row gap="4px" alignItems="center">
+        {icon && (
+        <IconWrapper
+          height={iconSize || '14px'}
+          width={iconSize || '14px'}
+          icon={icon}
+        />
+        )}
+        <Text size={labelFontSize ?? '12-Regular'} color={!isDisabled && hover ? textShades.SHADE_MINUS_3 : labelColor || (isDisabled ? textShades.SHADE_MINUS_1 : textShades.SHADE_MINUS_2)}>
           <>
-            <Row gap="4px" alignItems="center">
-              {l.icon && (
-              <IconWrapper
-                height={l.iconSize || '14px'}
-                width={l.iconSize || '14px'}
-                icon={l.icon}
-              />
-              )}
-              {l.label}
-            </Row>
+            {label}
             {required && (<Required disabled={!!isDisabled}>*</Required>)}
           </>
         </Text>
+      </Row>
+      )}
+      {labels && labels.map((l) => (
+        <Row key={l.label} gap="4px" alignItems="center">
+          {l.icon && (
+          <IconWrapper
+            height={l.iconSize || '14px'}
+            width={l.iconSize || '14px'}
+            icon={l.icon}
+          />
+          )}
+          <Text size={l?.fontSize ?? '12-Regular'} color={!isDisabled && hover ? textShades.SHADE_MINUS_3 : l.color ?? textShades.SHADE_MINUS_1}>
+            <>
+              {l.label}
+              {required && (<Required disabled={!!isDisabled}>*</Required>)}
+            </>
+          </Text>
+        </Row>
       )) }
       <div onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
         <Select
